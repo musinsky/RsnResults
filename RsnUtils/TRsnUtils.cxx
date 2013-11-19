203f5ef7b6b5ad55c27b9e45229502b5fde3befb
Jan Musinsky <musinsky@gmail.com>
2013-11-19 12:29:50 +0100

    function RangeFragments

--- RsnUtils/TRsnUtils.cxx
+++ RsnUtils/TRsnUtils.cxx
@@ -1,79 +1,72 @@
 // Authors: Jan Musinsky (jan.musinsky@cern.ch)
 //          Martin Vala  (martin.vala@cern.ch)
-// Date:    2013-11-14
+// Date:    2013-11-19
 
 #include <TH1.h>
 
 #include "TRsnUtils.h"
 
 //______________________________________________________________________________
-TArrayI *TRsnUtils::RangeFragments(const TH1 *h, Double_t range, Double_t min, Double_t max)
+TArrayI TRsnUtils::RangeFragments(const TH1 *h, Double_t range, Double_t min, Double_t max,
+                                  Bool_t round)
 {
   if (!h) return 0;
-
   TAxis *axis = h->GetXaxis();
-  if (axis->IsVariableBinSize()) {
-    Error("TRsnUtils::RangeFragments", "x-axis of histo: %s with variable bin size", h->GetName());
-    //    return 0;
-  }
-
-  Double_t rangemin = axis->GetBinWidth(-1);
-  Double_t rangemax = rangemin*axis->GetNbins();
-  if      (range < rangemin) range = rangemin;
-  else if (range > rangemax) range = rangemax;
-  Int_t brange = TMath::Nint(range/rangemin);
-  range = brange*rangemin; // nearest multiple of rangemin
 
   Int_t bmin = 1;
   Int_t bmax = axis->GetNbins();
   if (max > min) {
-    // minimum
-    min = TMath::Nint(min/range)*range; // nearest multiple of range
-    if (min > axis->GetXmin()) {
-      bmin = axis->FindFixBin(min);
-      if (axis->GetBinUpEdge(bmin) <= min) bmin += 1; // see TAxis::SetRangeUser
-    }
-    // maximum
-    max = TMath::Nint(max/range)*range;
-    if (max < axis->GetXmax()) {
-      bmax = axis->FindFixBin(max);
-      if (axis->GetBinLowEdge(bmax) >= max) bmax -= 1;
-    }
+    // without under/overflow
+    bmin = TMath::Max(bmin, axis->FindFixBin(min)); // include min
+    bmax = TMath::Min(bmax, axis->FindFixBin(max)); // exclude max
   }
 
-  brange -= 1; // important
-  if (brange > (bmax - bmin)) {
-    Error("TRsnUtils::RangeFragments",
-          "range of interval %g is greater than himself interval (%g, %g)", range, min, max);
-    return 0;
-  }
-
-  Int_t ifirst = bmin;
-  Int_t ilast = ifirst + brange;
+  Int_t first[axis->GetNbins()];
+  Int_t last[axis->GetNbins()];
   Int_t count = 0;
-  Int_t tmpFirst[axis->GetNbins()];
-  Int_t tmpLast[axis->GetNbins()];
-  tmpFirst[count] = ifirst;
-  tmpLast[count] = ilast;
-  while (ilast < (bmax - brange)) {
-    ifirst = ilast + 1;
-    ilast = ifirst + brange;
+  for (Int_t ib = bmin; ib <= bmax; ib++) {
+    Double_t width = axis->GetBinWidth(ib);
+    Int_t nwidth = ib;
+    while ((width < range) && !AreEqual(width, range)) // don't compare directly
+      width += axis->GetBinWidth(++nwidth);
+
+    if (round) // rounding range
+      if ((width-range) > (axis->GetBinWidth(nwidth)/2.0)) nwidth -= 1;
+    nwidth -= ib;
+    if (nwidth < 0) nwidth = 0; // special case
+
+    if ((ib+nwidth) > bmax) break;
+    first[count] = ib;
+    last[count]  = ib + nwidth;
     count++;
-    tmpFirst[count] = ifirst;
-    tmpLast[count] = ilast;
+    ib += nwidth;
   }
 
-  count += 1; // from index to size
-  TArrayI *array = new TArrayI(2*count);
+  TArrayI array(2*count);
   for (Int_t i = 0; i < count; i++) {
-    array->AddAt(tmpFirst[i], i);
-    array->AddAt(tmpLast[i], count + i);
-    Double_t le = axis->GetBinLowEdge(array->At(i));
-    Double_t ue = axis->GetBinUpEdge(array->At(count + i));
-    Printf("[%03d] = %03d, [%03d] = %03d   (%f, %f) %f", i, array->At(i), count + i, array->At(count + i),
-           le, ue, (le+ue)/2.0);
+    array[i]       = first[i];
+    array[count+i] = last[i];
   }
-  Printf("range = %f   min = %f   max =%f", range, axis->GetBinLowEdge(bmin), axis->GetBinUpEdge(bmax));
-  Printf("%f   %f", min, max);
   return array;
 }
+//______________________________________________________________________________
+void TRsnUtils::RangeFragmentsPrint(const TH1 *h, const TArrayI array)
+{
+  if (!h) return;
+  TAxis *axis = h->GetXaxis();
+
+  Int_t size = array.GetSize()/2;
+  Int_t first, last = 0;
+  Double_t low, up;
+  for (Int_t i = 0; i < size; i++) {
+    first = array[i];
+    if (i > 0 && ((first-last) != 1))
+      Error("TRsnUtils::RangeFragmentsPrint", "array must be contiguous");
+    last  = array[size+i];
+    low   = axis->GetBinLowEdge(first);
+    up    = axis->GetBinUpEdge(last);
+    Printf("([%03d] = %03d, [%03d] = %03d)  %d  (%f, %f) %f   \t %.4f", i, first, size+i, last,
+           last-first, low, up, (low+up)/2.0, up-low);
+  }
+}
+//______________________________________________________________________________
--- RsnUtils/TRsnUtils.h
+++ RsnUtils/TRsnUtils.h
@@ -1,19 +1,22 @@
 // Authors: Jan Musinsky (jan.musinsky@cern.ch)
 //          Martin Vala  (martin.vala@cern.ch)
-// Date:    2013-11-14
+// Date:    2013-11-19
 
 #ifndef RSNUTILS_H
 #define RSNUTILS_H
 
 #include <TMath.h>
+#include <TArrayI.h>
 
-class TArrayI;
 class TH1;
 
 namespace TRsnUtils {
 
   inline Bool_t  AreEqual(Double_t a, Double_t b, Double_t eps = 1.E-8);
-  TArrayI       *RangeFragments(const TH1 *h, Double_t range, Double_t min = 0., Double_t max = 0.);
+
+  TArrayI        RangeFragments(const TH1 *h, Double_t range, Double_t min = 0., Double_t max = 0.,
+                                Bool_t round = kFALSE);
+  void           RangeFragmentsPrint(const TH1 *h, const TArrayI array);
 }
 
 inline Bool_t TRsnUtils::AreEqual(Double_t a, Double_t b, Double_t eps) {
